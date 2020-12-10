9e4e1833a20e245f5c232bbe9246ddca1e75df94
MeliiZa <melissazandman@icloud.com>
2020-12-10 14:12:34 -0800

    Add quotes for motivation

---
+++ index.js
@@ -0,0 +1,38 @@
+(function () {
+  var quotes = [
+    {
+      quote:
+        "YOU CAN DO ANYTHING BUT NOT EVERYTHING",
+      author: "",
+    },
+    {
+      quote: "WHAT'S STOPPING YOU?",
+      author: "",
+    },
+    {
+      quote:
+        "EVERYDAY IS A FRESH START",
+      author: "",
+    },
+    { quote: "STAY POSITIVE.", author: "" },
+    {
+      quote:
+        "YOU ARE SO LOVED",
+      author: "",
+    },
+    {
+      quote:
+        "SUCCESS IS A SERIES OF SMALL WINS",
+      author: "",
+    },
+    {
+      quote:
+        "DO IT NOW YOU WILL THANK YOURSELF LATER",
+      author: "",
+    },
+    {
+      quote:
+        "BIG IDEAS HAVE SMALL BEGINNINGS",
+      author: "",
+    },
+  ];
